2502ef138f2e7942f0530fda7ad3bdb50881e226
Sandeepru <156737787+Sandeepru@users.noreply.github.com>
2024-04-04 13:41:46 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "lb_dns_name" {
  description = " arn:aws:elasticloadbalancing:us-west-2:533267178928:loadbalancer/app/alb/a1f8610e38cc03b7"
  value       = "alb-335123258.us-west-2.elb.amazonaws.com"
+}
